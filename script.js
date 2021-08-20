92c7deb337334d3aa09f2fd29523bc56168ca760
matthew156 <matthewastro@gmail.com>
2021-08-20 17:51:30 -0400

    Loop Challenges 1st Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+Problem 1
+for (var i = 1; i < 21; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+
+Problem 2
+for (i = 100; i > -1; i--) {
+  if (i % 3 === 0) {
+    console.log(i);
+  }
+}
+Problem 3
+let x = 4;
+for (i = 1; i < 6; i++) {
+  console.log(x);
+  x -=1.5;
+}
+Problem 4
+for (i = 1; i < 101; i++) {
+  sum+=i
+  console.log(sum);
+}
+
+Problem 5
+let product = i;
+for (i = 1; i < 13; i++) {
+  product *= i;
+}
+console.log(product);
